eb112413c85c92e84d1995d0d2034ff67da06ead
Oscar Bréhier <68658005+OscarBrehier@users.noreply.github.com>
2025-10-10 23:26:05 +0200

    config and write file

--- index.js
+++ index.js
@@ -5,47 +5,106 @@ import path from "path";
 const PORT_MIN = process.env.PORT_MIN ? Number(process.env.PORT_MIN) : 3000;
 const PORT_MAX = process.env.PORT_MAX ? Number(process.env.PORT_MAX) : PORT_MIN + 600;
 
-async function getConfiguration(filepath, placeholders) {
+class Portly {
 
-	let availablePort;
+	constructor(portEnvName, templatePath, domain, placeholders) {
 
-	portfinder.setBasePort(PORT_MIN);
-	portfinder.setHighestPort(PORT_MAX);
+		this.portEnvName = portEnvName;
+		this.templatePath = templatePath;
+		this.domain = domain;
+		this.placeholders = placeholders;
+
+		this.content = null;
+		this.__dirname = path.dirname(new URL(import.meta.url).pathname);
+
+	};
+
+	async getConfiguration() {
+
+		let availablePort;
+
+		portfinder.setBasePort(PORT_MIN);
+		portfinder.setHighestPort(PORT_MAX);
+
+		try {
+			availablePort = await portfinder.getPortPromise();
+		} catch (err) {
+			throw new Error(`Error finding available port: ${err}`);
+		};
+
+		if (!availablePort) return this;
+
+		console.log("Available port found:", availablePort);
+
+		process.env[this.portEnvName] = availablePort;
+		console.log(`export ${this.portEnvName}=${availablePort}`);
+
+		try {
+
+			const templateFullPath = path.join(this.__dirname, this.templatePath);
+			const data = await fs.promises.readFile(templateFullPath, "utf-8");
+
+			let content = data;
+
+			this.placeholders.forEach(([key, value]) => {
+				content = content.replaceAll(`{{${key}}}`, key === "PORT" ? availablePort : value);
+			});
+
+			this.content = content;
+			return this;
+			
+		} catch (err) {
+
+			throw new Error(`Error reading template file: ${err}`);
+
+		};
+
+	};
+
+	async writeConfigFile() {
+
+		if (!this.content) return null;
+
+		const configDir = path.join(this.__dirname, "./nginx-configs");
+		const nginxConfigFilePath = path.join(configDir, this.domain);
+
+		try {
+
+			await fs.promises.mkdir(configDir, { recursive: true });
+			await fs.promises.writeFile(nginxConfigFilePath, this.content, { encoding: "utf-8" });
+
+			console.log(`Config written to ${nginxConfigFilePath}`);
+
+		} catch (err) {
+
+			throw new Error(`Cannot write Nginx config: ${err}`);
+
+		};
+
+	};
+};
+
+(async () => {
 
 	try {
-		availablePort = await portfinder.getPortPromise();
+
+		const portly = new Portly(
+			process.env.PORT_ENV_NAME || "PORT",
+			"./nginx-template.txt",
+			"chat.eggspank.cloud",
+			[
+				["DOMAIN", "chat.eggspank.cloud"],
+				["PORT", ""]
+			]
+		);
+
+		await portly.getConfiguration();
+		await portly.writeConfigFile();
 	} catch (err) {
-		console.error("Error finding available port:", err);
-		return null;
-	}
 
-	if (!availablePort) return null;
+		console.error(`Portly execution failed: ${err}`);
+		process.exit(1);
 
-	try {
+	};
 
-		const __dirname = path.dirname(new URL(import.meta.url).pathname);
-		const templatePath = path.join(__dirname, filepath);
-
-		const data = await fs.promises.readFile(templatePath, "utf-8");
-
-		let content = data;
-		placeholders.forEach(([key, value]) => {
-			content = content.replaceAll(`{{${key}}}`, key === "PORT" ? availablePort : value);
-		});
-
-		return content;
-	} catch (err) {
-		console.error("Error reading template file:", err);
-		return null;
-	}
-}
-
-const config = await getConfiguration(
-	"./nginx_template.txt",
-	[
-		["DOMAIN", "chat.eggspank.cloud"],
-		["PORT", ""]
-	]
-);
-
-console.log(config);
+})();
